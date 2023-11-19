b773d675eb7274fb9fe8a65d42a607e5e1abdc10
AlanD20 <aland20@pm.me>
2023-11-19 10:51:29 +0100

    add dev config files

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+export const plugins = {
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
+    tailwindcss: {},
+    autoprefixer: {},
+};
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+import preset from "./vendor/filament/filament/tailwind.config.preset";
+
+export const presets = [preset];
+export const content = [
+    "./src/**/*.php",
+    "./resources/views/**/*.blade.php",
+    "./vendor/filament/**/*.blade.php",
+];
